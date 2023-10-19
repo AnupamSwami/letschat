59414c5686a409cbf9ab63162b405c277076e60c
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-10-19 18:58:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,51 +1,45 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyCON4_KJjzDzYse9iMuDhMX21GvHYZLaJc",
-  authDomain: "letschat-web-app-6fee6.firebaseapp.com",
-  databaseURL: "https://letschat-web-app-6fee6-default-rtdb.firebaseio.com",
-  projectId: "letschat-web-app-6fee6",
-  storageBucket: "letschat-web-app-6fee6.appspot.com",
-  messagingSenderId: "112361581383",
-  appId: "1:112361581383:web:36e0a7d31208e3ca3bba62"
+  apiKey: "AIzaSyAoWC5CzurS6CuWlLeJy1x-r1q8OTy-bTs",
+  authDomain: "letschatwebapplication.firebaseapp.com",
+  databaseURL: "https://letschatwebapplication-default-rtdb.firebaseio.com",
+  projectId: "letschatwebapplication",
+  storageBucket: "letschatwebapplication.appspot.com",
+  messagingSenderId: "762706695831",
+  appId: "1:762706695831:web:c84c88674cd3b9e8e7bbd2"
 };
 
 // Initialize Firebase
- firebase.app = initializeApp(firebaseConfig);
-
-user_name = document.getElementById("user_name").value ;
-room_name = document.getElementById("room_name").value ;
-document.getElementById("user").innerHTML = "Welcome " + user_name + " !"  ;
-
+firebase.initializeApp(firebaseConfig);
 
 function signin() {
-    localStorage.setItem("User Name" , user_name) ;
-    localStorage.setItem("Room Name" , room_name) ;
+  user_name = document.getElementById("user_name").value ;
+room_name = document.getElementById("room_name").value ;
+    localStorage.setItem("user_name" , user_name) ;
+    localStorage.setItem("room_name" , room_name) ;
     mmmbtn = " <div id='btn'> <button onclick='addRoom()' class='btn btn-primary w-100 py-2     mbtn' id='mbtm'> Add Room </button> </div> " ;
     document.getElementById("btn").innerHTML = mmmbtn ;
 } 
+ user_name = localStorage.getItem("user_name") ;
+ room_name = localStorage.getItem("room_name") ;
 
 function addRoom() {
-  room_name = localStorage.getItem("Room Name") ;
-  user_name = localStorage.getItem("User Name") ;
+  room_name = localStorage.getItem("room_name") ;
+  user_name = localStorage.getItem("user_name") ;
   firebase.database().ref("/").child(room_name).update({
     purpose : "Adding Room Name"
 }) ;
+    localStorage.setItem("user_name" , user_name) ;
+    localStorage.setItem("room_name" , room_name) ;
 window.location = "chat_page.html" ;
 }
 
 function logout() {
-    localStorage.removeItem("User Name") ;
-    localStorage.removeItem("Room Name") ;
-    localStorage.removeItem("Message") ;
+    localStorage.removeItem("user_name") ;
+    localStorage.removeItem("room_name") ;
 
     window.location = "index.html" ;
 }
 
-
-  function send_msg() {
-    message = document.getElementById("msg").value ;
-    document.getElementById("msg").value = "" ;
-    localStorage.setItem("Message" , message) ;
-  }
 
   function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
     Room_names = childKey;
