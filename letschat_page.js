9a16017f9e1ff4b8199c68b2160cb5276ca00e38
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-10-22 14:31:34 +0530

    Add files via upload

--- letschat_page.js
+++ letschat_page.js
@@ -10,14 +10,22 @@ var firebaseConfig = {
     
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+    room_name = localStorage.getItem("room_name") ;
+    user_name = localStorage.getItem("user_name") ;
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-console.log("Room Name =" + Room_names) ;
-row = "<div id="+Room_names+" class='room_name' onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr></hr> " ;
-document.getElementById("output").innerHTML+=row ;
+console.log(firebase_message_id) ;
+console.log(message_data) ;
+ var name = message_data['name'] ;
+ message = message_data['message'] ;
+ name_with_tag = " <h4> "+ name +" <img class='user_tick' src='tick.png'></h4> "
+ message_with_tag = " <h4 class='message_h4'>" + message +" </h4> "  ;
+
+ row = name_with_tag + message_with_tag ;
+ document.getElementById("output").innerHTML += row ;
 //End code
       } });  }); }
 getData();
@@ -27,7 +35,24 @@ function send() {
       firebase.database().ref(room_name).push({
             name:user_name ,
             message:msg ,
-            like:0
       }) ;
       document.getElementById("msg").value = "" ;
+}
+
+function updateLike(message_id) {
+      console.log("clicked on like button - " + message_id) ;
+      button_id = message_id ;
+      likes = document.getElementById("button_id").value ;
+      updated_likes = Number(likes) + 1 ;
+      console.log(updated_likes) ;
+
+      firebase.database().ref(room_name).child(message_id).update({
+            like : updated_likes
+      }) ;
+}
+
+function logout() {
+      localStorage.removeItem("user_name") ;
+      localStorage.removeItem("room_name") ;
+      window.location.replace("index.html") ;
 }
